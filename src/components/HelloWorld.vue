c0d70b1b35f9a05dbc2bf940de1392d309d9a760
hang <a1317374840@163.com>
2025-05-06 15:29:19 +0800

    不使用脚手架初始化项目: vue3 + ts

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="hello">
+    <h2>{{ msg }}</h2>
+    <p>
+      这是一个手动搭建的Vue 3 + Vite + TypeScript项目
+      <br>
+      没有使用任何脚手架工具
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.hello {
+  margin-bottom: 20px;
+}
+
+h2 {
+  color: #42b883;
+}
+</style>
